395540fc70df223fb12f4b1f422350274383988a
mcadm <mcadm@fccl-homecomputing.com>
2025-04-10 16:45:47 +0900

    * remove unused settings relating finit. * remove unused code. * add config dir in docker-compose.yml.

--- plugins/ncrtbuild_mastertype_docker.d/containers/ncrt-naemon/startup.sh
+++ plugins/ncrtbuild_mastertype_docker.d/containers/ncrt-naemon/startup.sh
@@ -4,51 +4,9 @@ env > /tmp/startup.env
 
 ##
 
-if [ "$SERVERSIDE_PORT" = "" ] ; then
-	SERVERSIDE_PORT=80
+if [ "`ls /etc/default/`" = "" ] ;then
+	rsync -aSvx /etc/default_orig/ /etc/default/
 fi
-
-if [ "$SERVERSIDE_PATH_PREFIX" = "" ] ; then
-	SERVERSIDE_PATH_PREFIX=/
-fi
-test "${SERVERSIDE_PATH_PREFIX##*/}" != "" && SERVERSIDE_PATH_PREFIX=$SERVERSIDE_PATH_PREFIX/
-
-if [ "$NAEMON2INFLUX_OUTPUT" = "" ] ; then
-	NAEMON2INFLUX_OUTPUT=http://ncrt-influxdb:8086/
-fi
-test "${NAEMON2INFLUX_OUTPUT##*/}" != "" && NAEMON2INFLUX_OUTPUT=$NAEMON2INFLUX_OUTPUT/
-if [ "$NAEMON2INFLUX_ORG" = "" ] ; then
-	NAEMON2INFLUX_ORG=ncrt_org
-fi
-if [ "$NAEMON2INFLUX_BUCKET" = "" ] ; then
-	NAEMON2INFLUX_BUCKET=ncrt_bucket
-fi
-if [ "$NAEMON2INFLUX_TOKEN" = "" ] ; then
-	NAEMON2INFLUX_TOKEN=ncrt_token
-fi
-
-if [ "$GDH_LISTENPORT" = "" ] ; then
-	GDH_LISTENPORT=46846
-fi
-if [ "$GDH_GRAFANAURL" = "" ] ; then
-	GDH_GRAFANAURL=http://ncrt-grafana:3000/
-fi
-test "${GDH_GRAFANAURL##*/}" != "" && GDH_GRAFANAURL=$GDH_GRAFANAURL/
-if [ "$GDH_GRAFANATOKEN" = "" ] ; then
-	GDH_GRAFANATOKEN=XXXXSECRETXXXX
-fi
-if [ "$GDH_GRAFANADATASOURCE" = "" ] ; then
-	GDH_GRAFANADATASOURCE=influxdb
-fi
-if [ "$GDH_INFLUXDBURL" = "" ] ; then
-	GDH_INFLUXDBURL=$NAEMON2INFLUX_OUTPUT
-fi
-test "${GDH_INFLUXDBURL##*/}" != "" && GDH_INFLUXDBURL=$GDH_INFLUXDBURL/
-if [ "$GDH_INFLUXDBBUCKET" = "" ] ; then
-	GDH_INFLUXDBBUCKET=$NAEMON2INFLUX_BUCKET
-fi
-
-##
 
 # setup apache2
 if [ "`ls /etc/apache2/`" = "" ] ;then
